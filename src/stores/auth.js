5da41e17ae299c11e7ac9fb005a8ac15f65ca401
Ali <aliw1382@gmail.com>
2025-01-04 23:42:07 +0330

    fixed bug

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -5,7 +5,7 @@ export const useAuthStore = defineStore( 'auth' , () =>
 {
     const phone = ref( '' )
     const password = ref( '' )
-    const iranianPhoneRegex = "/^09\d{9}$/";
+    const iranianPhoneRegex = /^09\d{9}$/;
     const isAuth = ref( false )
 
     const users = reactive( {
@@ -13,11 +13,11 @@ export const useAuthStore = defineStore( 'auth' , () =>
     } );
 
     function validationPhone() {
-        return iranianPhoneRegex.test( phone )
+        return iranianPhoneRegex.test( phone.value )
     }
 
     function checkPhoneExists() {
-        return phone in users;
+        return phone.value in users;
     }
 
     function reset() {
